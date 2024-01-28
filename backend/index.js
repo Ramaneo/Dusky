ae9a9c262c84d7c04260ae9fc74cc851777356a6
Hakkı Zambak <139214005+hakkizambak@users.noreply.github.com>
2024-01-28 14:46:00 +0300

    Created firebase connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// CS308 project copied
+const { initializeApp } = require('firebase/app');
+const { getDatabase, ref, get } = require('firebase/database');
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDS_a38npCwNGHDxPB90wYmLWgNYgdED_s",
+  authDomain: "dusky-c6a08.firebaseapp.com",
+  databaseURL: "https://dusky-c6a08-default-rtdb.firebaseio.com",
+  projectId: "dusky-c6a08",
+  storageBucket: "dusky-c6a08.appspot.com",
+  messagingSenderId: "34361181680",
+  appId: "1:34361181680:android:9fcb8287dc00ebfd158935"
+};
+
+
+//CS308 project copied
+// Initialize Firebase app
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseDb = ref(getDatabase(firebaseApp));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
